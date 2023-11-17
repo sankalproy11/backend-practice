39f5a63a9d2af20cfcee8e509816d10b736f5283
sankalproy11 <sankalproy11@gmail.com>
2023-11-17 19:15:31 +0530

    dir update blog

--- blog-app/index.js
+++ blog-app/index.js
@@ -3,9 +3,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 const connectDB = require("./config/database");
+const blogRoutes = require("./routes/blog");
 
 connectDB();
 app.use = express.json();
+
+(app.use = "/api/v1"), blogRoutes;
 
 app.get("/", (req, res) => {
   res.send("Blog App Home Route");
